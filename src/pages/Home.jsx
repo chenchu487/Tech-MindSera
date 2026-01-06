714250fc6cf698ca27d9a7749694edaac6cdbbd8
chenchu487 <yeturuchenchaiah96@gmail.com>
2026-01-06 23:47:37 +0530

    same error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Hero from "../components/Hero/Hero";
 import ServicesOverview from "../components/ServicesOverview/ServicesOverview";
 import WhyChooseUs from "../components/WhyChooseus/WhyChooseUs.jsx";
-import HowWeWork from "../components/How-we-work/HowWeWork";
-import StudyAbroad from "../components/Study-Abroad/StudyAbroad";
-import ProjectsInternships from "../components/Projects-Internships/ProjectsInternships";
-import Stats from "../components/stats/Stats";
-import Testimonials from "../components/Testimonials/Testimonials";
-import FinalCTA from "../components/Final-CTA/FinalCTA";
+import HowWeWork from "../components/How-we-work/HowWeWork.jsx";
+import StudyAbroad from "../components/Study-Abroad/StudyAbroad.jsx";
+import ProjectsInternships from "../components/Projects-Internships/ProjectsInternships.jsx";
+import Stats from "../components/stats/Stats.jsx";
+import Testimonials from "../components/testimonials/Testimonials.jsx";
+import FinalCTA from "../components/Final-CTA/FinalCTA.jsx";
 
 
 function App() {
