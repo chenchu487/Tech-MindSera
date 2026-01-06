b0eb4ffba0f8432795f6c9a6a8d2111bd4d54909
chenchu487 <yeturuchenchaiah96@gmail.com>
2026-01-07 00:02:27 +0530

    Header chsnges

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import "./header.css";
+import "../Header/Header.css";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/Asset.svg";
